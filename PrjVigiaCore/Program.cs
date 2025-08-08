4a1305e2f61183ecb0607703f5062a8a01c0ec6c
J0seJuli0 <josejuliosanchezcruzado1@gmail.com>
2025-08-08 11:25:53 -0500

    update chache

--- PrjVigiaCore/Program.cs
+++ PrjVigiaCore/Program.cs
@@ -25,6 +25,10 @@ app.Use(async (context, next) =>
     context.Response.Headers.Append("X-Frame-Options", "DENY");
     context.Response.Headers.Append("X-XSS-Protection", "1; mode=block");
 
+    context.Response.Headers["Cache-Control"] = "no-cache, no-store, must-revalidate";
+    context.Response.Headers["Pragma"] = "no-cache";
+    context.Response.Headers["Expires"] = "0";
+
     await next();
 });
 
